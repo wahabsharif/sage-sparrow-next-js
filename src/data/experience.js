e4b58740f00f64b11971d5496d381b0139654441
wahabsharif <www.rex19@gmail.com>
2024-03-11 15:09:42 +0500

    bugs

--- src/data/experience.js
+++
@@ -1,3 +0,0 @@
-export const experience = {
-  title: "Experience you can trust, Service quality you can \n easily count on",
-};
--- src/data/findSolution.js
+++
@@ -1,5 +0,0 @@
-export const findSolution = {
-  text: "Consulting &amp; Finance Services Built Specifically for your Business.",
-  linkText: "Find Your Solution",
-  href: "/about",
-};
