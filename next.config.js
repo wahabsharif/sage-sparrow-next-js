34398c69f1261b78ab79f23d4c7f16b7c66ca932
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-04-15 17:57:21 +0500

    Build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      // Add more export paths as needed
+    };
+  },
+  output: "export", // Add this line
 };
 
 module.exports = nextConfig;
